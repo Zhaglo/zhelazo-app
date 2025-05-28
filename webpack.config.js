144b14cc4974f72722f1007945dbe12d96757e2c
zhaglo <jagloik@gmail.com>
2025-05-28 18:50:45 +0300

    feat: webpack

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true,
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            }
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index.html',
+        }),
+    ],
+    devServer: {
+        static: path.resolve(__dirname, 'public'),
+        historyApiFallback: true,
+        open: true,
+        port: 3000,
+    },
+    mode: 'development',
+};
