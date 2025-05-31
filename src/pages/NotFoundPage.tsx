ca5116ab5804489467346611dd0fed31ac928055
zhaglo <jagloik@gmail.com>
2025-05-31 13:29:19 +0300

    fix: new zhelazo-style

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,5 +1,15 @@
-const NotFoundPage = () => {
-    return <div>404 - Page Not Found</div>;
-};
+import { Link } from "react-router-dom";
+import styles from "./NotFoundPage.module.scss";
+
+const NotFoundPage = () => (
+    <div className={styles.wrapper}>
+        <h1 className={styles.code}>404</h1>
+        <p className={styles.text}>Упс, страница не найдена :(</p>
+
+        <Link to="/dashboard" className={styles.homeButton}>
+            На главную
+        </Link>
+    </div>
+);
 
 export default NotFoundPage;
